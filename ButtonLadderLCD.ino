7fb61c8999d74ae93ad2bb21a7c74e2ffebcc7a7
Thomas Uitdewillegen <T.Uitdewillegen@students.uu.nl>
2017-03-04 15:46:03 +0100

    ButtonLadderLCD en Temperature
    Begin

---
+++ ButtonLadderLCD.ino
@@ -0,0 +1,122 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(0,1,2,3,4,5);
+
+const int buttonPin = A0;
+const int ledR = 9;
+const int ledG = 10;
+const int ledB = 11; 
+
+const int BUTTON1 = 1;
+const int BUTTON2 = 2;
+
+const int BUTTON1LOW = 750; //left button
+const int BUTTON1HIGH = 890;
+const int BUTTON2LOW = 900; //right button
+const int BUTTON2HIGH = 1100;
+
+int buttonState;
+int lastButtonState;
+int choice = 0;
+int counter = 0;
+
+int brightnessR = LOW;
+int brightnessG = LOW;
+int brightnessB = LOW;
+
+long lastDebounceTime = 0;
+long debounceDelay = 50;
+
+void setup(){
+  pinMode(buttonPin, INPUT);
+  //lcd.begin(16,2);
+  //state0();
+  Serial.begin(9600);
+}
+
+void loop(){
+  //analogWrite(ledR, brightnessR);
+  //analogWrite(ledG, brightnessG);
+  //analogWrite(ledB, brightnessB);
+
+  int reading = analogRead(buttonPin);
+
+  Serial.println(reading);
+/*
+  
+  int tmpButtonState = LOW;
+  
+  if(reading>BUTTON2LOW && reading<BUTTON2HIGH){
+    //Read switch 2
+    tmpButtonState = BUTTON2;
+  }else if(reading>BUTTON1LOW && reading<BUTTON1HIGH){
+    //Read switch 1
+    tmpButtonState = BUTTON1;
+  }else{
+    tmpButtonState = LOW;
+  }
+  
+  // check to see if you just pressed the button 
+  // (i.e. the input went from LOW to a buttonState),  and you've waited 
+  // long enough since the last press to ignore any noise:  
+
+  // If the switch changed, due to noise or pressing:
+  if (tmpButtonState != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+    if (tmpButtonState == BUTTON2){
+      counter = counter + 1; 
+    }
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+    buttonState = tmpButtonState;
+  }
+
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = tmpButtonState;
+
+  if(buttonState == BUTTON2){
+    
+    choice = counter % 3;
+  
+    switch(choice){
+      case 0:
+      state0(); 
+      break;
+      case 1:
+      state1();
+      break;
+      case 2:
+      state2();
+    }
+  }
+  */
+}
+
+void state0(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Temp:       C");
+  lcd.setCursor(6, 0);
+  lcd.print(calculateTemp());
+}
+
+void state1(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Schermpie");
+  lcd.setCursor(0,1);
+  lcd.print("twee");
+}
+
+void state2(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("LCD");
+  lcd.setCursor(0,1);
+  lcd.print("drie");
+}
---
+++ Temperature.ino
@@ -0,0 +1,59 @@
+#include <OneWire.h>
+
+// DS18S20 Temperature chip i/o
+OneWire ds(18);  // on pin 18/A4
+int HighByte, LowByte, TReading, SignBit, Tc_100, Whole, Fract;
+
+String calculateTemp(){
+  //OneWire
+  byte i;
+  byte present = 0;
+  byte data[12];
+  byte addr[8];
+  
+  ds.reset_search();
+  if ( !ds.search(addr)) {
+      //Serial.print("No more addresses.\n");
+      ds.reset_search();
+      return;
+  }
+  
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44,1);         // start conversion, with parasite power on at the end
+
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+  }  
+  LowByte = data[0];
+  HighByte = data[1];
+  TReading = (HighByte << 8) + LowByte;
+  SignBit = TReading & 0x8000;  // test most sig bit
+  if (SignBit) // negative
+  {
+    TReading = (TReading ^ 0xffff) + 1; // 2's comp
+  }
+  Tc_100 = (6 * TReading) + TReading / 4;    // multiply by (100 * 0.0625) or 6.25
+
+  Whole = Tc_100 / 100;  // separate off the whole and fractional portions
+  Fract = Tc_100 % 100;
+
+  char buffer1 [4];
+  char buffer2 [4];
+  itoa(Whole, buffer1, 10);
+  itoa(Fract, buffer2, 10);
+  
+  String res = String(buffer1);
+  res = res + ".";
+  res = res + String(buffer2);
+
+  return res;
+}
+
